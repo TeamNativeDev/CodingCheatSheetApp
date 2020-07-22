8c8ce13d8b13c522683f5afeaf7d469a2ef00460
biagioo <mendolia2328@gmail.com>
2020-07-22 11:41:59 -0400

    minor text change

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hey </Text>
+      <Text>Flatiorn Cheet Sheet </Text>
       <StatusBar style="auto" />
     </View>
   );
