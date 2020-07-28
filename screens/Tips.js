d5126c1f6b56df7ddda03dcaca03d2cae551a03c
reddevilcero <reddevil_cero@hotmail.com>
2020-07-28 20:00:16 +0100

    working solution, need to move to a component

--- screens/Tips.js
+++ screens/Tips.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, FlatList, TextInput } from 'react-native';
+import { View, StyleSheet, FlatList, TextInput, Text } from 'react-native';
 import Tip from '../components/Tip';
 
 const Tips = ({ route }) => {
@@ -7,6 +7,37 @@ const Tips = ({ route }) => {
   const [tips, setTips] = useState([]);
   const [input, setInput] = useState('');
   const [filter, setFilter] = useState(tips);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const styles = StyleSheet.create({
+    floatLabel: {
+      paddingHorizontal: 5,
+      paddingTop: 18,
+      marginBottom: 10,
+    },
+    label: {
+      position: 'absolute',
+      left: 10,
+      top: isFocused ? 0 : 18,
+      fontSize: isFocused ? 16 : 20,
+      color: isFocused ? '#000' : '#555',
+    },
+    input: {
+      paddingHorizontal: 5,
+      fontSize: 20,
+      borderBottomWidth: 2,
+      borderBottomColor: isFocused ? 'blue' : 'black',
+    },
+    container: {
+      paddingHorizontal: 10,
+    },
+
+    headerText: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginBottom: 10,
+    },
+  });
 
   useEffect(() => {
     fetch(`https://flatiron-cheat-sheet.herokuapp.com/api/v1/categories/${id}`)
@@ -29,8 +60,17 @@ const Tips = ({ route }) => {
 
   return (
     <View style={styles.container}>
-      <View>
-        <TextInput onChangeText={(text) => setInput(text)} value={input} />
+      <View style={styles.floatLabel}>
+        <Text style={styles.label}>
+          {isFocused ? "Let's discover!!" : 'Search your Favorite Tip'}
+        </Text>
+        <TextInput
+          onChangeText={(text) => setInput(text)}
+          value={input}
+          style={styles.input}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
       </View>
       <FlatList
         data={filter}
@@ -40,18 +80,5 @@ const Tips = ({ route }) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 10,
-    paddingTop: 30,
-  },
-
-  headerText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-});
 
 export default Tips;
