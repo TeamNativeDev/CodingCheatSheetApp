df1831550b9a747b88b7af420a52d868d0903f14
biagioo <mendolia2328@gmail.com>
2020-07-28 10:22:41 -0400

    removed tips preview from Category file

--- components/Category.js
+++ components/Category.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import TipsPreview from './TipsPreview';
 
 const Category = (props) => {
   const { title, color } = props;
   const containerColor = {
     backgroundColor: color,
   };
+
   return (
     <View style={[styles.categoryBox, containerColor]}>
       <Text style={styles.boxText}>{title}</Text>
-      <TipsPreview />
-      {/* for the tips preview, would need to pass the tips as a prop to the tips
-      preview component, from this comoponents props as long as the tips for 
-      each category is a nested array in the category from the backend */}
     </View>
   );
 };
