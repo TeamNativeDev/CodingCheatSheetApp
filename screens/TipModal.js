312d7fb26be70c03d5b0f1e0c646c0168f18ead1
biagioo <mendolia2328@gmail.com>
2020-07-30 08:53:05 -0400

    create vanilla TipModal component

---
+++ screens/TipModal.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+const TipModal = () => {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  );
+};
+
+export default TipModal;
