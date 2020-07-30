5dd2b2cdd56a848bea20c8b7950205b23872449c
reddevilcero <reddevil_cero@hotmail.com>
2020-07-30 18:59:19 +0100

    add floatLabel to tip Modal

--- screens/TipModal.js
+++ screens/TipModal.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
+import FloatLabelInput from '../components/FloatLabelInput';
 
 const TipModal = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [snippet, setSnippet] = useState('');
   return (
     <View>
       <Text>Hello world</Text>
+      <FloatLabelInput value={title} setValue={setTitle} mainLabel="Title" />
+      <FloatLabelInput
+        value={description}
+        setValue={setDescription}
+        mainLabel="Description"
+        multiline={true}
+        numberOfLines={5}
+      />
+      <FloatLabelInput
+        value={snippet}
+        setValue={setSnippet}
+        mainLabel="Snippet"
+        multiline={true}
+        numberOfLines={5}
+        code={true}
+      />
     </View>
   );
 };
