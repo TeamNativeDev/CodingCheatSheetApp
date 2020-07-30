f0a3187760976feec9d3a68d3c0939d50edd9862
reddevilcero <reddevil_cero@hotmail.com>
2020-07-30 18:59:01 +0100

    change float label behaivour for reusabitily

--- components/FloatLabelInput.js
+++ components/FloatLabelInput.js
@@ -3,18 +3,24 @@ import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 const FloatLabelInput = (props) => {
   const [isFocused, setIsFocused] = useState(false);
-  const { value, setValue, mainLabel, secondLabel = mainLabel } = props;
+  const {
+    value,
+    setValue,
+    mainLabel,
+    secondLabel = mainLabel,
+    code = false,
+  } = props;
 
   const styles = StyleSheet.create({
     floatLabel: {
       paddingHorizontal: 5,
-      paddingTop: 18,
+      paddingTop: 25,
       marginBottom: 10,
     },
     label: {
       position: 'absolute',
       left: 10,
-      top: isFocused ? 0 : 18,
+      top: isFocused ? 0 : 25,
       fontSize: isFocused ? 16 : 20,
       color: isFocused ? '#000' : '#555',
     },
@@ -23,12 +29,15 @@ const FloatLabelInput = (props) => {
       fontSize: 20,
       borderBottomWidth: 2,
       borderBottomColor: isFocused ? 'blue' : 'black',
+      textAlignVertical: 'top',
+      backgroundColor: code ? 'black' : null,
     },
   });
   return (
     <View style={styles.floatLabel}>
       <Text style={styles.label}>{isFocused ? secondLabel : mainLabel}</Text>
       <TextInput
+        {...props}
         onChangeText={(text) => setValue(text)}
         value={value}
         style={styles.input}
