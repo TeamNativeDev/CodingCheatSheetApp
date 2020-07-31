13b8f06a0161bc1e05aed8277c316e9d2a0c1352
reddevilcero <reddevil_cero@hotmail.com>
2020-07-31 12:36:56 +0100

    add improved FloatLabelInput avoid keyboard overlap

--- screens/TipModal.js
+++ screens/TipModal.js
@@ -1,15 +1,14 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet } from 'react-native';
 import FloatLabelInput from '../components/FloatLabelInput';
+import AppButton from '../components/AppButton';
 
 const TipModal = () => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [snippet, setSnippet] = useState('');
   return (
-    <View>
-      <Text>Hello world</Text>
+    <KeyboardAvoidingView style={styles.container} behavior={'padding'}>
       <FloatLabelInput value={title} setValue={setTitle} mainLabel="Title" />
       <FloatLabelInput
         value={description}
@@ -26,30 +25,16 @@ const TipModal = () => {
         numberOfLines={5}
         code={true}
       />
-    </View>
+      <AppButton onPress={() => console.warn('hello')}>
+        Send, It will go on Rails
+      </AppButton>
+    </KeyboardAvoidingView>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default TipModal;
-=======
-import React, { useState, useEffect, useCallback } from 'react';
-import { View, Text } from 'react-native';
-import FloatLabelInput from '../components/FloatLabelInput';
-
-const TipModal = () => {
-  const [title, setTitle] = useState('');
-  return (
-    <View>
-      <Text>Hello world</Text>
-      <FloatLabelInput
-        value={title}
-        setValue={setTitle}
-        mainLabel="Tip Title"
-        secondLabel="Think of Something Unique!"
-      />
-    </View>
-  );
-};
-
-export default TipModal;
->>>>>>> a05b711c5c7e7e9c06a2294185ed76f110994b9b
