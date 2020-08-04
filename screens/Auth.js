1e20f93f254ddb565c5175a4656b48fadb69db1a
lorrydriveloper <github email address>
2020-08-04 20:43:07 +0100

    add fetch to DB and return json

--- screens/Auth.js
+++ screens/Auth.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
 import Login from '../components/Login';
+import Profile from '../components/Profile';
+import * as SecureStore from 'expo-secure-store';
 
 const Auth = () => {
   const [isLogin, setIsLogin] = useState(false);
-  const [user, setUser] = useState({});
-  return isLogin ? <Profile /> : <Login />;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (user) {
+      const configObject = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({ user }),
+      };
+      fetch('http://localhost:3000/api/v1/login', configObject)
+        .then((res) => console.log(res))
+        // .then((json) => SecureStore.setItemAsync('user', json))
+        .then(setIsLogin(true));
+      // fetch(
+      //   'https://flatiron-cheat-sheet.herokuapp.com/api/v1/login',
+      //   configObject,
+      // )
+    }
+  }, [user]);
+
+  return isLogin ? <Profile /> : <Login setUser={setUser} />;
 };
 
 export default Auth;
