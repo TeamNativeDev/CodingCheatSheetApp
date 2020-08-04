ab825033f83c809c46b21271ab7a7b9f9e88338c
lorrydriveloper <github email address>
2020-08-04 21:13:20 +0100

    profile show User after Login

--- components/Profile.js
+++ components/Profile.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import * as SecureStore from 'expo-secure-store';
 
 const Profile = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function getUSer() {
+      const userData = await SecureStore.getItemAsync('user');
+      const userObject = await JSON.parse(userData);
+      setUser(userObject.user);
+    }
+
+    getUSer();
+  }, []);
+
   return (
-    <View>
+    <SafeAreaView>
       <Text>User Profile</Text>
-    </View>
+      <Text>{user?.username}</Text>
+    </SafeAreaView>
   );
 };
 
