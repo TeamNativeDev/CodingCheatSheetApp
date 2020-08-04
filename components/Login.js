a18c9cfb9e6ee8f5ea034773661eaf0958258126
lorrydriveloper <lorrydriveloper@gmail.com>
2020-08-04 20:42:41 +0100

    Login logic added

--- components/Login.js
+++ components/Login.js
@@ -3,23 +3,26 @@ import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FloatLabelInput from './FloatLabelInput';
 import AppButton from './AppButton';
-import * as SecureStore from 'expo-secure-store';
-const Login = ({ navigation }) => {
-  const [email, setEmail] = useState('');
+
+const Login = ({ setUser }) => {
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-
   const handleSubmit = () => {
-    SecureStore.setItemAsync('userId', 'id');
-    navigation.navigate('Home', { login: true });
+    setUser({ username: username.toLowerCase(), password });
   };
   return (
     <SafeAreaView>
-      <FloatLabelInput value={email} setValue={setEmail} mainLabel="Title" />
+      <FloatLabelInput
+        value={username}
+        setValue={setUsername}
+        mainLabel="Username"
+      />
       <FloatLabelInput
         value={password}
         setValue={setPassword}
-        mainLabel="Title"
+        mainLabel="Password"
+        secureTextEntry={true}
       />
       <AppButton onPress={handleSubmit}>Login</AppButton>
     </SafeAreaView>
