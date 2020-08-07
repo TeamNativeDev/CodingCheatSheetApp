cfab1bfd1cc0721e29f940fdd02d649f2c23dac7
lorrydriveloper <lorrydriveloper@gmail.com>
2020-08-07 23:14:03 +0100

    clean up moving MainStack

--- screens/Navigator.js
+++ screens/Navigator.js
@@ -1,60 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import Home from './Home';
-import Categories from './Categories';
 import Tips from './Tips';
 import { createStackNavigator } from '@react-navigation/stack';
 import TipModal from './TipModal';
 import AnimatedSplash from 'react-native-animated-splash-screen';
-import Auth from './Auth';
-import { Entypo } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { connect } from 'react-redux';
 import { fetchCategories } from '../actions/categoriesActions';
+import MainStackTabs from '../components/MainStackTabs';
 
 const RootStack = createStackNavigator();
-const Tab = createBottomTabNavigator();
 
 const Navigator = (props) => {
   const [categories, setCategories] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
-  const MainStackTabs = () => {
-    return (
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{
-            tabBarIcon: () => <Entypo name="home" size={24} color="black" />,
-          }}
-        />
-        <Tab.Screen
-          name="Categories"
-          initialParams={{ categories }}
-          component={Categories}
-          options={{
-            tabBarIcon: () => <Entypo name="list" size={24} color="black" />,
-          }}
-        />
-        <Tab.Screen
-          name={'Profile'}
-          component={Auth}
-          options={{
-            tabBarIcon: () => <Entypo name="user" size={24} color="black" />,
-          }}
-        />
-      </Tab.Navigator>
-    );
-  };
 
   useEffect(() => {
     props
       .fetchCategories()
       .then(() => {
-        console.log(props.categories);
+        console.log('prop: ', props.categories);
         setCategories(props.categories);
       })
-      .then(setIsLoaded(true));
+      .then(() => {
+        console.log('state: ', categories);
+        setIsLoaded(true);
+      });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
