40d35c623ebf2d1dd2145c88099cee68c28b1ef3
lorrydriveloper <lorrydriveloper@gmail.com>
2020-08-10 19:08:00 +0100

    implement complex login with Thunk and Redux

--- actions/authActions.js
+++ actions/authActions.js
@@ -1,6 +1,30 @@
-export const loginUser = () => ({
-  type: 'LOGIN',
-});
+import BASEURL from '../helpers/BaseUrl';
+import { Alert } from 'react-native';
+
+export const loginUser = (user) => {
+  const configurationObject = {
+    method: 'Post',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(user),
+  };
+  return async (dispatch) => {
+    const res = await fetch(BASEURL + 'login', configurationObject);
+    const json = await res.json();
+    if (json.error) {
+      // implement and alert or something here
+
+      Alert.alert(json.error + ' ' + json.message);
+    } else {
+      dispatch({
+        type: 'LOGIN',
+        payload: { user: json.user, jwt: json.jwt },
+      });
+    }
+  };
+};
 
 export const logoutUser = () => ({
   type: 'LOGOUT',
