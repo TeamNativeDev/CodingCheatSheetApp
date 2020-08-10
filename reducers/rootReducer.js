756981b28d5089e8230b117843858dc4cc24d54c
lorrydriveloper <lorrydriveloper@gmail.com>
2020-08-10 13:31:42 +0100

    combine new authReducer

--- reducers/rootReducer.js
+++ reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import categoriesReducer from './categoriesReducer';
+import authReducer from './authReducer';
 
 const rootReducer = combineReducers({
   categoriesStore: categoriesReducer,
+  authStore: authReducer,
 });
 
 export default rootReducer;
