7a0a23224168a82cfa49a28e116e8849b43eaffe
biagioo <mendolia2328@gmail.com>
2020-08-11 09:03:51 -0400

    created Sign Up action

--- actions/authActions.js
+++ actions/authActions.js
@@ -29,3 +29,28 @@ export const loginUser = (user) => {
 export const logoutUser = () => ({
   type: 'LOGOUT',
 });
+
+export const signUpUser = (newUser) => {
+  const configurationObject = {
+    method: 'Post',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(newUser),
+  };
+  return async (dispatch) => {
+    const res = await fetch(BASEURL + 'sign_up', configurationObject);
+    const json = await res.json();
+    if (json.error) {
+      // implement and alert or something here
+
+      Alert.alert(json.error + ' ' + json.message);
+    } else {
+      dispatch({
+        type: 'LOGIN',
+        payload: { user: json.user, jwt: json.jwt },
+      });
+    }
+  };
+};
