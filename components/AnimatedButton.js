91f7eb8f9fe76215557aa8c3a3a22d6d0f415ee0
lorrydriveloper <lorrydriveloper@gmail.com>
2020-08-12 11:36:55 +0100

    create animated component

---
+++ components/AnimatedButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const AnimatedButton = () => {
+  return (
+    <View>
+      <Text> SignUp</Text>
+    </View>
+  );
+};
+
+export default AnimatedButton;
+
+const styles = StyleSheet.create({});
