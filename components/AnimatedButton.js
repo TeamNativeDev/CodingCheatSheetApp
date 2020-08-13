05e168950cf3c50752833b008ca8c80d41bdc01c
lorrydriveloper <lorrydriveloper@gmail.com>
2020-08-13 14:14:42 +0100

    Change color style

--- components/AnimatedButton.js
+++ components/AnimatedButton.js
@@ -8,7 +8,7 @@ const AnimatedButton = ({ animatedValue, onPress, flipped }) => {
   const inputRange = [0, 0.001, 0.5, 0.501, 1];
   const containerBg = animatedValue.interpolate({
     inputRange,
-    outputRange: ['gold', 'gold', 'gold', '#444', '#444'],
+    outputRange: ['#005086', '#005086', '#005086', '#318fb5', '#318fb5'],
   });
   const styles = StyleSheet.create({
     circleButton: {
@@ -20,14 +20,14 @@ const AnimatedButton = ({ animatedValue, onPress, flipped }) => {
       width: CIRCLE_SIZE,
       height: CIRCLE_SIZE,
       borderRadius: CIRCLE_SIZE / 4,
-      backgroundColor: '#444',
+      backgroundColor: '#318fb5',
     },
     container: {
       margin: 10,
       alignItems: 'center',
     },
     text: {
-      color: 'white',
+      color: '#f7d6bf',
       fontSize: 20,
       fontWeight: '900',
       transform: [
@@ -71,7 +71,7 @@ const AnimatedButton = ({ animatedValue, onPress, flipped }) => {
               {
                 translateX: animatedValue.interpolate({
                   inputRange: [0, 0.5, 1],
-                  outputRange: ['0%', '50%', '0%'],
+                  outputRange: [0, 5, 0],
                 }),
               },
             ],
@@ -83,7 +83,7 @@ const AnimatedButton = ({ animatedValue, onPress, flipped }) => {
             <Animated.Text style={styles.text}>
               {flipped ? 'Login' : 'Signup'}
             </Animated.Text>
-            <AntDesign name="arrowright" size={28} color={'white'} />
+            <AntDesign name="arrowright" size={28} color={'#f7d6bf'} />
           </View>
         </TouchableOpacity>
       </Animated.View>
