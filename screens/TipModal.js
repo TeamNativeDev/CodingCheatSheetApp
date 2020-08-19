5efb5c586dbd2f50ea2b6f220a19aba2b50d34c2
biagioo <mendolia2328@gmail.com>
2020-08-19 19:19:57 -0400

    accepted incoming changes

--- screens/TipModal.js
+++ screens/TipModal.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { View, Text } from 'react-native';
-import FloatLabelInput from '../components/FloatLabelInput';
-
-const TipModal = () => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [snippet, setSnippet] = useState('');
-  return (
-    <View>
-      <Text>Hello world</Text>
-      <FloatLabelInput value={title} setValue={setTitle} mainLabel="Title" />
-      <FloatLabelInput
-        value={description}
-        setValue={setDescription}
-        mainLabel="Description"
-        multiline={true}
-        numberOfLines={5}
-      />
-      <FloatLabelInput
-        value={snippet}
-        setValue={setSnippet}
-        mainLabel="Snippet"
-        multiline={true}
-        numberOfLines={5}
-        code={true}
-      />
-    </View>
-  );
-};
-
-export default TipModal;
-=======
 import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text } from 'react-native';
 import FloatLabelInput from '../components/FloatLabelInput';
@@ -52,4 +18,3 @@ const TipModal = () => {
 };
 
 export default TipModal;
->>>>>>> a05b711c5c7e7e9c06a2294185ed76f110994b9b
