9858a0d9ab46f571f4886081947ba9ba7fdcc553
biagioo <mendolia2328@gmail.com>
2020-08-30 14:01:39 -0400

    tested props.tip in a flatlist

--- components/Profile.js
+++ components/Profile.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FloatLabelInput from './FloatLabelInput';
 import { connect } from 'react-redux';
+import Tip from './Tip';
 
 const Profile = (props) => {
   const [username, setUsername] = useState(props.user.username);
   const [userEmail, setUserEmail] = useState(props.user.email);
   const [bio, setBio] = useState(props.user.bio);
+
   return (
     <SafeAreaView>
       <Text>User Profile</Text>
@@ -32,12 +34,23 @@ const Profile = (props) => {
         numberOfLines={3}
         autoFocus={false}
       />
+
+      <View>
+        <Text>My Tips</Text>
+      </View>
+
+      {/* <FlatList
+        data={props.tips}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Tip tip={item} />}
+      /> */}
     </SafeAreaView>
   );
 };
 const mapStateToProps = ({ authStore }) => ({
   user: authStore.user,
   jwt: authStore.jwt,
+  tips: authStore.tips,
 });
 
 export default connect(mapStateToProps)(Profile);
