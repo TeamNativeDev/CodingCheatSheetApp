8f3f4531f3da9207156ffab8f6b950cc63b7a8ce
lorrydriveloper <lorrydriveloper@gmail.com>
2020-09-01 16:03:27 +0100

    add new style to center header of tip

--- components/TipCardComponents/TipFrontRight.js
+++ components/TipCardComponents/TipFrontRight.js
@@ -3,33 +3,43 @@ import { borderRadius } from '../../styles/MainStyles';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import SyntaxHighlighter from 'react-native-syntax-highlighter';
 import { tomorrow } from 'react-syntax-highlighter/styles/prism';
+import { connect } from 'react-redux';
 
-const TipFrontRight = ({ onPress, code_snippet, title }) => {
+const TipFrontRight = ({
+  category_id,
+  onPress,
+  code_snippet,
+  title,
+  categories,
+}) => {
+  // console.log(categories, category_id);
   return (
-    <View style={{ alignSelf: 'center' }}>
-      <View>
-        <TouchableOpacity onPress={onPress}>
-          <Text style={rightViewStyle.headerText}>{title} </Text>
-
-          <Text style>Press to see the description</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={rightViewStyle.right_side}>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.header} onPress={onPress}>
+        <Text style={styles.headerText}>{title} </Text>
+        <Text style>Press to see the description</Text>
+      </TouchableOpacity>
+      <View style={styles.right_side}>
         <SyntaxHighlighter
           style={tomorrow}
-          customStyle={rightViewStyle.highlighter}
+          customStyle={styles.highlighter}
           language="javascript"
           fontSize={12}
           highlighter="prism"
         >
-          {code_snippet}
+          {code_snippet || 'nothing'}
         </SyntaxHighlighter>
       </View>
     </View>
   );
 };
 
-const rightViewStyle = StyleSheet.create({
+const styles = StyleSheet.create({
+  header: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: { alignSelf: 'center' },
   highlighter: {
     ...borderRadius,
     padding: 5,
@@ -55,4 +65,8 @@ const rightViewStyle = StyleSheet.create({
   },
 });
 
-export default TipFrontRight;
+const mapStateToProps = ({ categoriesStore }) => ({
+  categories: categoriesStore.categories,
+});
+
+export default connect(mapStateToProps)(TipFrontRight);
