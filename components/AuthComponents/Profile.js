1feb45b20b95d74297d6a4a26662b921fa3a6b8c
lorrydriveloper <lorrydriveloper@gmail.com>
2020-09-03 13:20:16 +0100

    add new Profile Screen styles

--- components/AuthComponents/Profile.js
+++ components/AuthComponents/Profile.js
@@ -1,47 +1,69 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, View, FlatList, StyleSheet, Image } from 'react-native';
 import { connect } from 'react-redux';
 import Tip from '../Tip';
 import Gradient from '../../helpers/Gradient';
 import { absoluteCenter } from '../../styles/MainStyles';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Profile = (props) => {
   const { user, categories } = props;
-  const findColor = (catArray, id) => {
+  const _findColor = (catArray, id) => {
     let output = catArray.find((cat) => cat.id === id);
     return output?.color;
   };
 
+  const _likes = (array) =>
+    array.reduce((acc, tip) => acc + tip.votes.length, 0);
+
+  useEffect(() => {
+    console.log(user);
+  }, [user);
+
   return (
-    <View style={styles.container}>
+    <>
       <Gradient />
-      <View style={styles.userContainer}>
-        <Image
-          style={styles.logo}
-          source={require('../../assets/profileIcon.png')}
-        />
-        <View>
-          <Text style={styles.username}>{user.username.toUpperCase()}</Text>
-          <View style={{ flexDirection: 'row' }}>
-            <Text>4 tipss</Text>
-            <Text>4 tipss</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.userContainer}>
+          <Image
+            style={styles.logo}
+            source={require('../../assets/profileIcon.png')}
+          />
+          <View style={absoluteCenter}>
+            <Text style={styles.username}>{user.username.toUpperCase()}</Text>
+            <View style={styles.stats}>
+              <View style={absoluteCenter}>
+                <Image
+                  style={styles.like}
+                  source={require('../../assets/code.png')}
+                />
+                <Text style={styles.text}>{user.tips.length} TIPS</Text>
+              </View>
+              <View style={absoluteCenter}>
+                <Image
+                  style={styles.like}
+                  source={require('../../assets/like.png')}
+                />
+                <Text style={styles.text}>{_likes(user.tips)} Likes</Text>
+              </View>
+            </View>
           </View>
         </View>
-      </View>
-      <View style={styles.tipsContainer}>
-        <FlatList
-          data={user.tips}
-          keyExtractor={(item) => item.title}
-          renderItem={({ item }) => (
-            <Tip
-              tip={item}
-              color={findColor(categories, item.category_id)}
-              {...props}
-            />
-          )}
-        />
-      </View>
-    </View>
+        <View style={styles.tipsContainer}>
+          <FlatList
+            data={user.tips}
+            keyExtractor={(item) => item.title}
+            renderItem={({ item }) => (
+              <Tip
+                tip={item}
+                color={_findColor(categories, item.category_id)}
+                {...props}
+              />
+            )}
+          />
+        </View>
+      </SafeAreaView>
+    </>
   );
 };
 const mapStateToProps = ({ authStore, categoriesStore }) => ({
@@ -56,18 +78,34 @@ const styles = StyleSheet.create({
   userContainer: {
     ...absoluteCenter,
     flex: 2,
-    backgroundColor: 'yellow',
   },
   tipsContainer: {
     flex: 3,
   },
   logo: {
-    width: 150,
-    height: 150,
+    width: 100,
+    height: 100,
+    marginVertical: 5,
   },
   username: {
     fontSize: 30,
     fontWeight: 'bold',
+  },
+  stats: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 250,
+    padding: 10,
+  },
+  like: {
+    height: 50,
+    width: 50,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '700',
+    fontStyle: 'italic',
+    padding: 10,
   },
 });
 
