f3c641f7de63469cc7df3d192fd24b030eed6b96
lorrydriveloper <lorrydriveloper@gmail.com>
2020-09-03 12:21:53 +0100

    place user icon

--- components/AuthComponents/Profile.js
+++ components/AuthComponents/Profile.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import { Text, View, FlatList, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, View, FlatList, StyleSheet, Image } from 'react-native';
 import { connect } from 'react-redux';
 import Tip from '../Tip';
 import Gradient from '../../helpers/Gradient';
+import { absoluteCenter } from '../../styles/MainStyles';
 
 const Profile = (props) => {
   const { user, categories } = props;
-  const findColor = (categories, id) => {
-    let output = categories.find((cat) => cat.id === id);
+  const findColor = (catArray, id) => {
+    let output = catArray.find((cat) => cat.id === id);
     return output?.color;
   };
 
   return (
     <View style={styles.container}>
       <Gradient />
-      <View style={styles.userContainer} />
+      <View style={styles.userContainer}>
+        <Image
+          style={styles.logo}
+          source={require('../../assets/profileIcon.png')}
+        />
+        <View>
+          <Text style={styles.username}>{user.username.toUpperCase()}</Text>
+          <View style={{ flexDirection: 'row' }}>
+            <Text>4 tipss</Text>
+            <Text>4 tipss</Text>
+          </View>
+        </View>
+      </View>
       <View style={styles.tipsContainer}>
         <FlatList
           data={user.tips}
@@ -42,12 +54,20 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   userContainer: {
+    ...absoluteCenter,
     flex: 2,
     backgroundColor: 'yellow',
   },
   tipsContainer: {
     flex: 3,
-    backgroundColor: 'red',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+  },
+  username: {
+    fontSize: 30,
+    fontWeight: 'bold',
   },
 });
 
